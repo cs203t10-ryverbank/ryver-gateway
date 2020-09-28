b8f1e39f122740cc492c79f5a65033416f2117b7
Bryan Lee <bryanmylee@gmail.com>
2020-09-29 01:45:46 +0800

    Use ryver-auth service to transform requests with JWT

--- src/main/java/cs203t10/ryver/gateway/RyverBankGatewayApplication.java
+++ src/main/java/cs203t10/ryver/gateway/RyverBankGatewayApplication.java
@@ -2,15 +2,15 @@ package cs203t10.ryver.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 import cs203t10.ryver.gateway.filter.AuthFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableDiscoveryClient
 public class RyverBankGatewayApplication {
 
 	public static void main(String[] args) {
@@ -20,6 +20,11 @@ public class RyverBankGatewayApplication {
     @Bean
     public AuthFilter authFilter() {
         return new AuthFilter();
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
     }
 
 }
--- src/main/java/cs203t10/ryver/gateway/filter/AuthFilter.java
+++ src/main/java/cs203t10/ryver/gateway/filter/AuthFilter.java
@@ -1,12 +1,32 @@
 package cs203t10.ryver.gateway.filter;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient
 public class AuthFilter extends ZuulFilter {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Override
     public String filterType() {
@@ -20,6 +40,13 @@ public class AuthFilter extends ZuulFilter {
 
     @Override
     public boolean shouldFilter() {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+        String path = request.getServletPath();
+        // Login route should not be filtered.
+        if (path.startsWith("/login")) {
+            return false;
+        }
         return true;
     }
 
@@ -27,10 +54,33 @@ public class AuthFilter extends ZuulFilter {
     public Object run() throws ZuulException {
         RequestContext requestContext = RequestContext.getCurrentContext();
         HttpServletRequest request = requestContext.getRequest();
+        String authHeader = request.getHeader("Authorization");
 
-        System.out.println(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+        // If the request uses Basic authentication, generate and append a JWT
+        // using the ryver-auth service.
+        if (authHeader != null && authHeader.startsWith("Basic ")) {
+            String jwtToken = getJWTBearerToken(authHeader);
+            requestContext.addZuulRequestHeader("Authorization", jwtToken);
+        }
 
         return null;
+    }
+
+    private String getJWTBearerToken(String basicAuthHeader) {
+        // Find an instance of the auth service.
+        List<ServiceInstance> instances = discoveryClient.getInstances("ryver-auth");
+        String url = instances.get(0).getUri().toString();
+        String loginUrl = url + "login";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", basicAuthHeader);
+        HttpEntity<String> request = new HttpEntity<>("", headers);
+        try {
+            ResponseEntity<String> response = restTemplate.postForEntity(loginUrl, request, String.class);
+            return response.getHeaders().getFirst("Authorization");
+        } catch (RestClientException e) {
+            return "";
+        }
     }
 
 }
